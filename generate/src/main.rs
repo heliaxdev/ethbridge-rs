d9d27b1f5005221d8270c599edc3c4f5c3c5a54d
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-02-07 10:37:35 +0000

    Begin ABI gen code

---
+++ generate/src/main.rs
@@ -0,0 +1,23 @@
+use clap::Parser;
+
+/// Generate Ethereum bridge Rust types compatible with Namada's
+/// Rust code
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The path to the directory to parse ABI files from
+    #[arg(short = 'p', long, default_value_t = String::from("abi"))]
+    abi_files_dir: String,
+
+    /// The git tag of `ethereum-bridge` whose artifacts
+    /// we are to download. If no tag is provided, we try
+    /// to use files present in the specified ABI files
+    /// directory
+    #[arg(short = 't', long)]
+    ethereum_bridge_tag: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Parsed: {args:#?}");
+}
